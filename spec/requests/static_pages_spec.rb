4819ad7c5a19f996fd9dcf7585824e0993d1ba3f
Caleb Majo <calebmajo@yahoo.com>
2013-04-05 22:14:33 +0300

    Add fail tests for home,help, about. About still failing

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'static pages' do 
+
+	#Test for the Home Page
+	describe 'Home page' do
+		it "Should have content 'Sample App'" do
+			visit "/static_pages/home"
+			page.should have_content("Sample app")
+		end
+	end
+
+	#Test for the Help Page
+	describe 'Help page' do
+		it 'should have content "Help Page"' do
+			visit "/static_pages/help"
+			page.should have_content('help')
+		end
+	end
+
+
+	#Test for the About page
+	describe "About page" do
+		it "should have content 'About Us'" do
+			visit "/static_pages/about"
+			page.should have_content('About Us')
+		end
+	end
+	
+end

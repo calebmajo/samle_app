9a7c16fb8606a3b34b9382d40f9f2cae8c96a006
Caleb Majo <calebmajo@yahoo.com>
2013-04-16 19:12:41 +0300

    Finish User Signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name		"Juma Hamisi"
+		email		"juma@example.com"
+		password	"foobar"
+		password_confirmation "foobar"
+	end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -73,6 +73,17 @@
 		 it { should_not be_valid }
 		end
 
+		describe "email adress with mixed case" do
+			let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+			it "should be saved as all lower-case" do
+				@user.email = mixed_case_email
+				@user.save
+				@user.reload.email.should == mixed_case_email.downcase
+			end
+		end
+
+
 		describe "When password is not present" do
 			before { @user.password = @user.password_confirmation = " " }
 			it { should_not be_valid }

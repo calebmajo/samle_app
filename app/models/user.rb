d9c205cfce1f13f2fcad3b75eeea3e641005088d
Caleb Majo <calebmajo@yahoo.com>
2013-04-16 20:15:36 +0300

    Complete Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,6 @@ class User < ActiveRecord::Base
    validates :name , presence: true, length: { maximum: 50 }
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-   validates :password, presence: true, length: { minimum: 6 }
+   validates :password, length: { minimum: 6 }
    validates :password_confirmation, presence: true
 end
